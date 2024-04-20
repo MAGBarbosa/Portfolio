fdbb9455a6cf88381ed04e380cc873fed0abc6de
MAGBarbosa <138673494+MAGBarbosa@users.noreply.github.com>
2024-04-20 11:24:30 +0100

    Added cards to my index page

---
+++ index.js
@@ -0,0 +1,14 @@
+<div>
+  My very first SheCodes project of a landing page created using HTML, CSS and
+  vanilla Javascript.
+</div>;
+
+<div>
+  A more advanced project from SheCodes Plus, based on API integration. Coded
+  using HTML, CSS, Bootstrap and vanilla Javascript.
+</div>;
+<div>
+  Project resulting of the SheCodes Plus Add-on course. Based on real time in
+  various countries, using Moment.js library. Coded using HTML, CSS, and vanilla
+  Javascript.
+</div>;
