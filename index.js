d741740e416eb18bfbd4329dcfb80d6dc9cbcb52
MAGBarbosa <138673494+MAGBarbosa@users.noreply.github.com>
2024-04-30 12:38:34 +0100

    Fixed back button in app-card; added styling to contact form

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-/*
-<div>
-  My very first SheCodes project of a landing page created using HTML, CSS and
-  vanilla Javascript.
-</div>;
-
-<div>
-  A more advanced project from SheCodes Plus, based on API integration. Coded
-  using HTML, CSS, Bootstrap and vanilla Javascript.
-</div>;
-<div>
-  Project resulting of the SheCodes Plus Add-on course. Based on real time in
-  various countries, using Moment.js library. Coded using HTML, CSS, and vanilla
-  Javascript.
-</div>;
-*/
 let app1 = document.getElementById("app1");
 let app2 = document.getElementById("app2");
 let app3 = document.getElementById("app3");
