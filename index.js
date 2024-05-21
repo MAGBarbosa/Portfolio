c2505fc817010720f7536d3b7e9586e7ae71d6f1
MAGBarbosa <138673494+MAGBarbosa@users.noreply.github.com>
2024-05-21 21:42:40 +0100

    Added functionality to the contact form

--- index.js
+++ index.js
@@ -1,3 +1,100 @@
+/*Send email through contact form */
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let subject = document.getElementById("subject");
+let bodyMessage = document.getElementById("message");
+let btn = document.getElementById("form-button");
+
+function sendEmail() {
+  let body =
+    `Name: ${name.value}` +
+    `<br/>` +
+    `Email: ${email.value}` +
+    `<br/>` +
+    `Subject: ${subject.value}` +
+    `<br/>` +
+    `Message: ${bodyMessage.value}`;
+
+  console.log("vou enviar mails: ", `${email.value}`);
+  Email.send({
+    SecureToken: "6e30ab4f-d162-4d07-8f97-fd52de36a0b6",
+    To: "melanie_barbosa@hotmail.com",
+    From: "melanie_barbosa@hotmail.com",
+    Subject: subject.value,
+    Body: body,
+  }).then((message) => {
+    console.log(message);
+    if (message == "OK") {
+      alert("Message sent successfully!");
+    } else {
+      alert(
+        "Something went wrong. Please contact me through an alternative method."
+      );
+    }
+  });
+}
+
+/* Check inputs in contact form */
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+    if (items[1].value != "") {
+      checkEmail();
+    }
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+function checkEmail() {
+  const emailRegex = /^([a-z\d.-]+)@([a-z\d.-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  const errorTxtEmail = document.querySelector(".error-txt.email");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+
+    if (email.value != "") {
+      errorTxtEmail.innerText = "Enter a valid email address";
+    } else {
+      errorTxtEmail.innerText = "Email Address can't be blank";
+    }
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (
+    !name.classList.contains("error") &&
+    !email.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !bodyMessage.classList.contains("error")
+  ) {
+    sendEmail();
+
+    form.reset();
+    return false;
+  }
+});
+
+/* flip card animation */
 let app1 = document.getElementById("app1");
 let app2 = document.getElementById("app2");
 let app3 = document.getElementById("app3");
